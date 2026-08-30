2cee99c915c9409568b4ed2cfae63c4d5d2cf632
Gagandeep <gagandeep@Gagandeeps-MacBook-Air.local>
2026-08-30 12:45:37 +0530

    Fix CTA contrast and education imagery

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import "./button-contrast.css";
 import "./trust.css";
 import "./internal-links.css";
 import "./case-studies.css";
